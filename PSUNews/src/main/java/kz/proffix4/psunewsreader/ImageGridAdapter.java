3120a00486eeed453e8ac8ca4aef508c748ecd2f
dev-gvs <44653764+dev-gvs@users.noreply.github.com>
2021-04-17 19:46:04 +0600

    Update dependencies, migrate to AndroidX

--- PSUNews/src/main/java/kz/proffix4/psunewsreader/ImageGridAdapter.java
+++ PSUNews/src/main/java/kz/proffix4/psunewsreader/ImageGridAdapter.java
@@ -1,7 +1,7 @@
 package kz.proffix4.psunewsreader;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +9,6 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ImageGridAdapter extends RecyclerView.Adapter<ImageGridAdapter.GridItemViewHolder> {
